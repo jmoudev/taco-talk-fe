c79796aaba81d4391727191a6eb2be8188d5668f
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2021-02-16 10:06:18 +0000

    created basic title and component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Title from './components/Title';
+import LoginPage from './components/LoginPage';
+import ChatPage from './components/ChatPage';
+import { Router } from '@reach/router';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title />
+      <Router>
+        <LoginPage />
+        <ChatPage />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Title() {
+  return (
+    <header className="title">
+      <h1>🌮 Taco Talk 🌮</h1>
+      <h2>This is a chat app for tacos and their enthusiasts</h2>
+    </header>
+  );
+}
