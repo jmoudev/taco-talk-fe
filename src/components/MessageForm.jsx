82a1861bf6a0e52d2a136b314145f7f82c682b90
lchapmb <lchapmb@googlemail.com>
2021-02-16 14:36:21 +0000

    adds handleChange to message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,12 +1,19 @@
 import React, { PureComponent } from 'react';
 
 class MessageForm extends PureComponent {
+  state = { message: '' };
+
   handleSend = (event) => {
     event.preventDefault();
+    console.log(event.target.value);
     // if (input.value) {
     //   socket.emit('chat apricot', input.value);
     //   input.value = '';
     // }
+  };
+
+  handleChange = (event) => {
+    this.setState({ message: event.target.value });
   };
 
   render() {
@@ -14,7 +21,7 @@ class MessageForm extends PureComponent {
       <div className='messageForm'>
         <h3>Submit Message</h3>
         <form id='form' action=''>
-          <label id='input' autoComplete='off'>
+          <label onChange={this.handleChange} id='input' autoComplete='off'>
             <textarea></textarea>
           </label>
           <button onClick={this.handleSend}>Send</button>
